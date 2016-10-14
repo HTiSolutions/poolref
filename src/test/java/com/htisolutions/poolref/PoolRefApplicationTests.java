c5b68e3d9513807869dc06be79b2e255bd233fb4
Bradley Miles <bm15731@my.bristol.ac.uk>
2016-10-14 13:56:34 +0100

    Initial commit

---
+++ src/test/java/com/htisolutions/poolref/PoolRefApplicationTests.java
@@ -0,0 +1,16 @@
+package com.htisolutions.poolref;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PoolRefApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
