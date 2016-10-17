d6d935a451539944e84fea38b0205012a2be2413
Bradley Miles <bradley.miles@qton.com>
2016-10-17 13:22:17 +0100

    Feature/implement first spring views (#1)
    * Implement first spring views
    - Add static home page
    - Implement GreetingController
    - Update gitignore
    - Update build.gradle
    
    * - Fix build.gradle
    - Update imported libraries for controllers
    
    * Test travis.yml refactor
    
    * Implement build script with gradlew

--- src/main/java/com/htisolutions/poolref/PoolRefApplication.java
+++ src/main/java/com/htisolutions/poolref/PoolRefApplication.java
@@ -1,12 +1,13 @@
 package com.htisolutions.poolref;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
 
 @SpringBootApplication
+@EnableAutoConfiguration
 public class PoolRefApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PoolRefApplication.class, args);
+        SpringApplication.run(PoolRefApplication.class, args);
 	}
 }
---
+++ src/main/java/com/htisolutions/poolref/controllers/GreetingController.java
@@ -0,0 +1,19 @@
+package com.htisolutions.poolref.controllers;
+
+import org.springframework.ui.Model;
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class GreetingController {
+
+    @RequestMapping("/greeting")
+    public String greeting(
+            @RequestParam(value="name", required=false, defaultValue="World") String name,
+            Model model
+    ) {
+        model.addAttribute("name", name);
+        return "greeting";
+    }
+
+}
