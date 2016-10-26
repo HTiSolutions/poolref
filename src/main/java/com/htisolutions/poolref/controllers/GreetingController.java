beb37ddc0acd5bba7ad84c2c506ac4cd52f09323
Gareth Carless <garethcarless@btinternet.com>
2016-10-26 14:20:50 +0100

    indentation and ignoring classes

--- src/main/java/com/htisolutions/poolref/controllers/GreetingController.java
+++ src/main/java/com/htisolutions/poolref/controllers/GreetingController.java
@@ -8,7 +8,8 @@ import org.springframework.web.bind.annotation.*;
 public class GreetingController {
 
     @RequestMapping("/greeting")
-    public String index(@RequestParam(
+    public String index(
+            @RequestParam(
                     value="name",
                     required=false,
                     defaultValue="World")
