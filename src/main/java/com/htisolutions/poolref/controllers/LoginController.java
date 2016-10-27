5ebb25c2ee9d1ef33d60556ff4dfc835aa68c1a4
GeorgeBall <gb15351@my.bristol.ac.uk>
2016-10-27 13:43:35 +0100

    Merged master and refactored controller

--- src/main/java/com/htisolutions/poolref/controllers/LoginController.java
+++ src/main/java/com/htisolutions/poolref/controllers/LoginController.java
@@ -15,29 +15,6 @@ public class LoginController {
 
     @RequestMapping("/signin")
     public String signIn(
-    @RequestMapping("/register")
-    public String register() {
-        return "register";
-    }
-
-    @RequestMapping("/greeting/registered")
-    public String registered(
-            @RequestParam(value = "first-name") String firstName,
-            @RequestParam(value = "last-name") String lastName,
-            @RequestParam(value = "register-email") String registerEmail,
-            @RequestParam(value = "register-password") String registerPassword,
-            @RequestParam(value = "confirm-password") String confirmPassword) {
-
-        if (registerPassword.equals(confirmPassword)) {
-            return "redirect:/greeting";
-        }
-        else {
-            return "redirect:/register";
-        }
-    }
-
-    @RequestMapping("/login/signin")
-    public String signin(
             @RequestParam(value="email") String email,
             @RequestParam(value="password") String password)
     {
--- src/main/java/com/htisolutions/poolref/controllers/RegisterController.java
+++ src/main/java/com/htisolutions/poolref/controllers/RegisterController.java
@@ -1,7 +1,31 @@
 package com.htisolutions.poolref.controllers;
 
-/**
- * Created by george on 27/10/2016.
- */
+import org.springframework.stereotype.*;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/register")
 public class RegisterController {
+
+    @RequestMapping()
+    public String index() {
+        return "register";
+    }
+
+    @RequestMapping("/register")
+    public String register(
+            @RequestParam(value = "first-name") String firstName,
+            @RequestParam(value = "last-name") String lastName,
+            @RequestParam(value = "register-email") String registerEmail,
+            @RequestParam(value = "register-password") String registerPassword,
+            @RequestParam(value = "confirm-password") String confirmPassword) {
+
+        if (registerPassword.equals(confirmPassword)) {
+            return "redirect:/greeting";
+        }
+        else {
+            return "redirect:/register";
+        }
+    }
 }
