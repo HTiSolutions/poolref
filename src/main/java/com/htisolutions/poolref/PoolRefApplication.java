bf14b1ce36c5b66f0503088935c972c2ffab3bc8
Bradley Miles <bm15731@my.bristol.ac.uk>
2016-11-13 16:16:57 +0000

    Implement first Db connection - Create User model - Create User Database Access Object - Add local Db to application configuration

--- src/main/java/com/htisolutions/poolref/PoolRefApplication.java
+++ src/main/java/com/htisolutions/poolref/PoolRefApplication.java
@@ -9,4 +9,5 @@ public class PoolRefApplication {
 	public static void main(String[] args) {
         SpringApplication.run(PoolRefApplication.class, args);
 	}
+
 }
---
+++ src/main/java/com/htisolutions/poolref/controllers/UserController.java
@@ -0,0 +1,52 @@
+package com.htisolutions.poolref.controllers;
+
+import com.htisolutions.poolref.models.User;
+import com.htisolutions.poolref.models.UserDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.View;
+import org.springframework.web.servlet.view.RedirectView;
+
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+    private UserDao userDao;
+
+    @Autowired
+    UserController(UserDao userDao) {
+        this.userDao = userDao;
+    }
+
+    @RequestMapping("")
+    public ModelAndView index(Long id,  Model model) {
+        User currentUser = userDao.findOne(id);
+
+        ModelAndView view = new ModelAndView("user/index");
+        view.addObject("user", currentUser);
+        return view;
+    }
+
+    @RequestMapping("/create")
+    public ModelAndView create(String forename, String surname) {
+
+        String userId = "";
+        try {
+            User user = new User(forename, surname);
+            userDao.save(user);
+            userId = String.valueOf(user.getId());
+        }
+        catch (Exception ex) {
+            //"Error creating the user: " + ex.toString();
+        }
+
+        ModelAndView view = new ModelAndView(new RedirectView(""));
+        view.addObject("id", userId);
+        return view;
+    }
+
+}
