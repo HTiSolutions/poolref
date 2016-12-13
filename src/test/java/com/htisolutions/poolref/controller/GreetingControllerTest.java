157d9511fc323022e05fd885465cf376dc64f68c
Gareth Carless <garethcarless@btinternet.com>
2016-12-13 12:27:49 +0000

    fixed tests

--- src/test/java/com/htisolutions/poolref/controller/GreetingControllerTest.java
+++ src/test/java/com/htisolutions/poolref/controller/GreetingControllerTest.java
@@ -13,10 +13,12 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.security.test.context.support.WithMockUser;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
+@WithMockUser
 public class GreetingControllerTest {
 
     @Autowired
