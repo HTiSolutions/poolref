b3898fe74c27bbcca9d619469c9625571016c7e2
GeorgeBall <gb15351@my.bristol.ac.uk>
2017-03-13 15:58:14 +0000

    League controller is now merged into one. Buttons to view and create leagues are working.

--- src/main/java/com/htisolutions/poolref/controllers/LeagueController.java
+++ src/main/java/com/htisolutions/poolref/controllers/LeagueController.java
@@ -2,24 +2,29 @@ package com.htisolutions.poolref.controllers;
 
 import com.htisolutions.poolref.entities.User;
 import com.htisolutions.poolref.services.SubLeaguesService;
+import com.htisolutions.poolref.services.UserService;
+import com.htisolutions.poolref.viewModels.CreateSubLeagueViewModel;
 import com.htisolutions.poolref.viewModels.LeaderBoardEntryViewModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
 @Controller
 @RequestMapping("/sub-leagues")
-public class SubLeaguesController {
+public class LeagueController {
 
     private SubLeaguesService subLeaguesService;
+    private UserService userService;
 
     @Autowired
-    SubLeaguesController(SubLeaguesService subLeaguesService) {
+    LeagueController(SubLeaguesService subLeaguesService, UserService userService) {
         this.subLeaguesService = subLeaguesService;
+        this.userService = userService;
     }
 
     @RequestMapping()
@@ -29,5 +34,26 @@ public class SubLeaguesController {
         ModelAndView model = new ModelAndView("views/sub-leagues");
         model.addObject("sub-leagues",subLeagues);
         return model;
+    }
+
+    @RequestMapping(value = "/create", method = RequestMethod.GET)
+    public ModelAndView createIndex(){
+
+        CreateSubLeagueViewModel createSubLeague = new CreateSubLeagueViewModel();
+        createSubLeague.setUsers(userService.getUsers());
+
+        //Provide empty form for user to fill in (will contain list of users)
+
+        return new ModelAndView("views/create-sub-league", "createLeague", createSubLeague);
+
+    }
+
+    @RequestMapping(value = "/create", method = RequestMethod.POST)
+    public ModelAndView create() {
+        // Take filled form data
+        // Create league in the Db
+
+
+        return new ModelAndView();
     }
 }
