c288c9b2465c79ba973adc7b7365374b21664351
Gareth Carless <garethcarless@btinternet.com>
2017-03-23 17:18:11 +0000

    added entity tag to SecurityQuestion

--- src/main/java/com/htisolutions/poolref/entities/SecurityQuestion.java
+++ src/main/java/com/htisolutions/poolref/entities/SecurityQuestion.java
@@ -1,11 +1,9 @@
 package com.htisolutions.poolref.entities;
 
-import javax.persistence.Column;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 
+@Entity
 public class SecurityQuestion {
 
     @Id
