1a747b40c5b52f3a90760711b6681e041276fd9e
Adam Stein <ams593@hotmail.co.uk>
2017-04-25 16:24:53 +0100

    fix sql

--- src/main/resources/migrations/migration20170415_twitter.sql
+++ src/main/resources/migrations/migration20170415_twitter.sql
@@ -1,15 +1,13 @@
-CREATE TABLE `pool_ref`.`security_question` (
+CREATE TABLE `pool_ref`.`twitter_token` (
   `id` INT(11) NOT NULL AUTO_INCREMENT,
-  `question` VARCHAR(80) NOT NULL,
+  `twitter_name` VARCHAR(45) NOT NULL,
+  `token` VARCHAR(80) NOT NULL,
+  `token_secret` VARCHAR(80) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE INDEX `id_UNIQUE` (`id` ASC));
+ENGINE = InnoDB
+AUTO_INCREMENT = 1
+DEFAULT CHARACTER SET = utf8;
 
 ALTER TABLE `pool_ref`.`user`
-ADD COLUMN `security_question_id` INT(11) NULL AFTER `hashedpassword`,
-ADD COLUMN `security_question_answer` VARCHAR(80) NULL AFTER `security_question_id`;
-
-ALTER TABLE `pool_ref`.`user`
-CHANGE COLUMN `hashedpassword` `hashed_password` VARCHAR(80) NOT NULL ;
-
-INSERT INTO `pool_ref`.`security_question` (id, question)
-VALUES ('1', "What is your mother's maiden name?"), ('2', "In what city were you born?"), ('3', "What secondary school did you attend?"), ('4', "What street did you grow up on?"), ('5', "What was the make of your first car?"), ('6', "Which is your favorite web browser?"), ('7', "What is the name of your favorite pet?");
+ADD COLUMN `twitter_token_id` INT(11) NULL AFTER `security_question_answer`
--- src/main/resources/migrations/pool_ref.sql
+++ src/main/resources/migrations/pool_ref.sql
@@ -25,16 +25,6 @@ ENGINE = InnoDB
 AUTO_INCREMENT = 1
 DEFAULT CHARACTER SET = utf8;
 
-CREATE TABLE IF NOT EXISTS `pool_ref`.`twitter_token` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT,
-  `twitter_name` VARCHAR(45) NOT NULL,
-  `token` VARCHAR(80) NOT NULL,
-  `token_secret` VARCHAR(80) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE INDEX `id_UNIQUE` (`id` ASC));
-ENGINE = InnoDB
-AUTO_INCREMENT = 1
-DEFAULT CHARACTER SET = utf8;
 -- -----------------------------------------------------
 -- Table `pool_ref`.`game`
 -- -----------------------------------------------------
